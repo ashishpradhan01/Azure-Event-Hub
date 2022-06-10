6b74b780d782d35137163f03d114bb89f6ce7b6d
Ashish Pradhan <ashishpradhan892@gmail.com>
2022-06-10 08:37:20 +0530

    completed event hub basic practical

---
+++ EventHub-Receive/Program.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHub_Receive
+{
+    class Program
+    {
+        //Single Partition
+        //private static readonly string conn_string = "Endpoint=sb://hubnamespace-ashish.servicebus.windows.net/;SharedAccessKeyName=Receive;SharedAccessKey=vXSEoViewcFtyu4mrOTh2FHbsAjAoD9m04v2zJ6Tyl8=;EntityPath=single-part";
+
+        //Multple Partition
+        //private static readonly string conn_string = "Endpoint=sb://hubnamespace-ashish.servicebus.windows.net/;SharedAccessKeyName=Common;SharedAccessKey=1tfFHq2kjsIAdbuZH+p6gRqTIeEUFsjChxcfDGljRck=;EntityPath=multiple-part";
+
+        //SQL Logs
+        private static readonly string conn_string = "Endpoint=sb://hubnamespace-ashish.servicebus.windows.net/;SharedAccessKeyName=DBLOG;SharedAccessKey=qEZl27YbQkpsd9XHYP3gmAW+Rj7OMqGkffqKds94eng=;EntityPath=dblogstream";
+
+        private static readonly string consumer_group = "$Default";
+        static async Task Main(string[] args)
+        {
+
+            EventHubConsumerClient hub_client = new EventHubConsumerClient(consumer_group, conn_string);
+
+            Console.WriteLine("Consuming events");
+
+            await foreach (PartitionEvent _event in hub_client.ReadEventsAsync())
+            {
+                Console.WriteLine("***********************************************");
+                Console.WriteLine($"Partition ID {_event.Partition.PartitionId}");
+                Console.WriteLine($"Data Offset {_event.Data.Offset}");
+                Console.WriteLine($"Sequence Number {_event.Data.SequenceNumber}");
+                Console.WriteLine($"Partition Key {_event.Data.PartitionKey}");
+                Console.WriteLine(Encoding.UTF8.GetString(_event.Data.EventBody));
+                Console.WriteLine("***********************************************\n");
+            }
+
+            //From specific parition
+            /*string PartitionId = (await hub_client.GetPartitionIdsAsync()).First();
+            await foreach (PartitionEvent _event in hub_client.ReadEventsFromPartitionAsync(PartitionId, EventPosition.Earliest))
+            {
+                Console.WriteLine("***********************************************");
+                Console.WriteLine($"Partition ID {_event.Partition.PartitionId}");
+                Console.WriteLine($"Data Offset {_event.Data.Offset}");
+                Console.WriteLine($"Sequence Number {_event.Data.SequenceNumber}");
+                Console.WriteLine($"Partition Key {_event.Data.PartitionKey}");
+                Console.WriteLine(Encoding.UTF8.GetString(_event.Data.EventBody));
+                Console.WriteLine("***********************************************");
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EventHub-SendMessages/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Messaging.EventHubs.Producer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHub_SendMessages
+{
+    class Program
+    {
+        //Single Partition - Event Hub
+        //private static readonly string conn_string = "Endpoint=sb://hubnamespace-ashish.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=UzcyTqjNMKUk2K0hUJKYdQICA3nuIM35epHSgCU/3rE=;EntityPath=single-part";
+
+        //Multiple Partition - Event Hub
+        private static readonly string conn_string = "Endpoint=sb://hubnamespace-ashish.servicebus.windows.net/;SharedAccessKeyName=Common;SharedAccessKey=1tfFHq2kjsIAdbuZH+p6gRqTIeEUFsjChxcfDGljRck=;EntityPath=multiple-part";
+        private static readonly bool multiple = true;
+        static void Main(string[] args)
+        {
+
+            EventHubProducerClient hub_client = new EventHubProducerClient(conn_string);
+
+            EventDataBatch hub_batch = hub_client.CreateBatchAsync().GetAwaiter().GetResult();
+
+            List<string> messages = new List<string>();
+
+            for (int i = 10; i < 41; i++)
+            {
+                if(multiple) messages.Add($"Hello message from Multiple Partition - {i}");
+                else messages.Add($"Hello message from Single Partition - {i}");
+            }
+        
+
+            foreach (string mgs in messages)
+            {
+                hub_batch.TryAdd(new Azure.Messaging.EventHubs.EventData(Encoding.UTF8.GetBytes(mgs)));
+                Console.WriteLine($"This [ {mgs} ] is sent successfully");
+            }
+
+            hub_client.SendAsync(hub_batch).GetAwaiter().GetResult();
+            Console.WriteLine("!! All messages are sent !!");
+        }
+    }
+}
